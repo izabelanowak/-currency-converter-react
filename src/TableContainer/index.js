8b01915b15bd4bf0a1ccb52498064e8ccaa12878
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-06-08 21:59:52 +0200

    unnessesary p removed

--- src/TableContainer/index.js
+++ src/TableContainer/index.js
@@ -3,9 +3,7 @@ import Table from "../Table";
 
 const tableContainer = ({ currencies }) => (
     <div className="tableContainer">
-        <p>
-            <button className="table__button"><span>Ukryj</span> aktualne kursy walut</button>
-        </p>
+        <button className="table__button"><span>Ukryj</span> aktualne kursy walut</button>
         <Table currencies={currencies} />
         <p className="table__paragraph">Srednie kursy walut aktualne na dzień 01.05.2021r.</p>
     </div>
