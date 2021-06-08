97b8d09057295d581d9d87faf628ce1b9f1f3086
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-06-08 21:48:22 +0200

    table render logic added

--- src/Table/index.js
+++ src/Table/index.js
@@ -1,33 +1,33 @@
 import "./style.css";
 
-const Table = (props) => (
+const Table = ({ currencies }) => (
     <table className="table">
         <caption className="table__caption">Aktualne kursy walut</caption>
-        <tr className="table__row">
-            <th className="table__cell table__cell--header" scope="col">Nazwa</th>
-            <th className="table__cell table__cell--header" scope="col">Waluta</th>
-            <th className="table__cell table__cell--header" scope="col">Kurs</th>
-        </tr>
-        <tr className="table__row">
-            <th className="table__cell table__cell--header" scope="row">Euro</th>
-            <td className="table__cell">EUR</td>
-            <td className="table__cell">4.5654</td>
-        </tr>
-        <tr className="table__row">
-            <th className="table__cell table__cell--header" scope="row">Funt brytyjski</th>
-            <td className="table__cell">GBP</td>
-            <td className="table__cell">5.2491</td>
-        </tr>
-        <tr className="table__row">
-            <th className="table__cell table__cell--header" scope="row">Dolar amerykański</th>
-            <td className="table__cell">USD</td>
-            <td className="table__cell">3.7746</td>
-        </tr>
-        <tr className="table__row">
-            <th className="table__cell table__cell--header" scope="row">Frank szwajcarski</th>
-            <td className="table__cell">CHF</td>
-            <td className="table__cell">4.1502</td>
-        </tr>
+        <thead>
+            <tr className="table__row">
+                <th className="table__cell table__cell--header" scope="col">Nazwa</th>
+                <th className="table__cell table__cell--header" scope="col">Waluta</th>
+                <th className="table__cell table__cell--header" scope="col">Kurs</th>
+            </tr>
+        </thead>
+        <tbody>
+            {currencies.map(currency => (
+                <tr className="table__row">
+                    <th className="table__cell table__cell--header" scope="row">
+                        {currency.name}
+                    </th>
+                    <td className="table__cell">
+                        {currency.id}
+                    </td>
+                    <td className="table__cell">
+                        {currency.exchangeRate}
+                    </td>
+                </tr>
+            ))}
+        </tbody>
+        <tfoot>
+            <p className="table__paragraph">Srednie kursy walut aktualne na dzień 01.05.2021r.</p>
+        </tfoot>
     </table>
 );
 
