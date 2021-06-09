016285659e03beef08f9e3acd3fd26f61a4bbdd9
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-06-09 22:49:34 +0200

    calculating result added, onFormSubmit added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,21 +1,35 @@
+import { useState } from "react";
 import "./style.css";
 
 const Form = ({ currencies }) => {
+    const [amount, setAmount] = useState(1);
+    const [selectedCurrency, setSelectedCurrency] = useState("Euro");
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const rate = currencies.find(({ name }) => name === selectedCurrency).exchangeRate;
+        const result = calculateResult(amount, rate);
+    };
+
+    const calculateResult = (amount, rate) => {
+        return amount / rate;
+    };
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={onFormSubmit}>
             <fieldset className="form__fieldset">
                 <legend className="form__legend">Kalkulator walut</legend>
                 <p>
                     <label className="form__label">
                         <span className="form__labelText">Kwota w złotych - PLN*:</span>
                         <input className="form__input" type="number" min="0" max="1000000000" step="0.01"
-                            required />
+                            required value={amount} onChange={({ target }) => setAmount(target.value)} />
                     </label>
                 </p>
                 <p>
                     <label className="form__label">
                         <span className="form__labelText">Waluta:</span>
-                        {<select className="form__select" name="currency">
+                        {<select className="form__select" value={selectedCurrency} onChange={({ target }) => setSelectedCurrency(target.value)}>
                             {currencies.map(currency => (
                                 <option key={currency.id}>
                                     {currency.name}
