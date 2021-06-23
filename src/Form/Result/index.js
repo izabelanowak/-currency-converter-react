d392f578749bdd51f96ca9c7177b874f14c6d925
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-06-23 13:30:50 +0200

    Result css changes to styled-componentes

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,10 +1,10 @@
-import "./style.css";
+import { ResultParagraph, Strong } from "./styled";
 
 const Result = ({ result }) => (
     result !== undefined && (
-        <p className="result">
-            Otrzymasz: <strong className="result--strong">{result.value.toFixed(2)}</strong> {result.currency}
-        </p>
+        <ResultParagraph className="result">
+            Otrzymasz: <Strong>{result.value.toFixed(2)}</Strong> {result.currency}
+        </ResultParagraph>
     )
 );
 
---
+++ src/Form/Result/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ResultParagraph = styled.p`
+    text-align: center;
+    padding: 15px;
+`;
+
+export const Strong = styled.strong`
+    color: #0e6746;
+`;
