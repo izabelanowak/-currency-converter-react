d1e67f4aaa7d78070278b360931590a9f2e62368
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-06-23 14:32:36 +0200

    Button imported from theme

--- src/Form/styled.js
+++ src/Form/styled.js
@@ -26,27 +26,3 @@ export const Input = styled.input`
     width: 100%;
     max-width: 300px;
 `;
-
-export const Button = styled.button`
-    width: 100%;
-    font-size: 22px;
-    color: #fff;
-    background-color: #0e6746;
-    padding: 15px;
-    border-radius: 10px;
-    margin-top: 10px;
-
-    &:focus {
-        outline: none;
-        border-color: black;
-    }
-
-    &:hover {
-        background-color: hsl(158, 76%, 30%);
-    }
-
-    &:active {
-        background-color: hsl(158, 76%, 40%);
-        outline: none;
-    }
-`;
