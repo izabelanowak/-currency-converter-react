84bd4b4f01285453b9ae4aab849995e4f10b6b9e
Izabela Nowak <nowak.izabela.ni@gmail.com>
2021-06-25 14:45:14 +0200

    Container code minimalized to styled-components code in index.js

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,9 +1,14 @@
-import { Wrapper } from "./styled";
+import styled from "styled-components";
+import background from "./background.jpg";
 
-const Container = ({ children }) => (
-    <Wrapper>
-        {children}
-    </Wrapper>
-);
+const Container = styled.main`
+    margin: 0 auto;
+    padding: 30px;
+    background-image: url("${background}");
+    background-size: cover;
+    background-position: bottom;
+    min-height: 100vh;
+    max-width: 100%;
+`;
 
 export default Container;
